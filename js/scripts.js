7e56178de52e31a4761e1a66c07dfc3c33cdb7fb
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-08-30 09:29:40 -0800

    Update new Code and end edit in this project

--- js/scripts.js
+++ js/scripts.js
@@ -1,149 +1,152 @@
 $(() => {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 30,
-    responsiveClass: true,
-    navText: [
-      '<span class="lnr lnr-arrow-up"></span>',
-      '<span class="lnr lnr-arrow-down"></span>',
-    ],
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-      },
-      600: {
-        items: 1,
-        nav: true,
-        margin: 0,
-      },
-      1000: {
-        items: 2,
-        nav: true,
-      },
-    },
-  });
-  new WOW().init();
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        responsiveClass: true,
+        navText: ['<span class="lnr lnr-arrow-up"></span>', '<span class="lnr lnr-arrow-down"></span>'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true,
+                margin: 0
+            },
+            1000: {
+                items: 2,
+                nav: true
+            }
+        }
+    });
+    new WOW().init();
 });
 
 let stickyTop = document.querySelector(".header-nav");
 function scrolled() {
-  stickyTop.classList.toggle("active", window.scrollY >= 200);
+    stickyTop.classList.toggle("active", window.scrollY >= 200);
 }
 window.addEventListener("scroll", scrolled);
 
-let bars = document.querySelector(".bars");
+let bars = document.querySelector(".header-nav .bars");
 let navSite = document.querySelector(".nav-site");
-let overlay = document.querySelector(".overlay-body");
 bars.onclick = function () {
-  this.classList.toggle("active");
-  if (this.classList.contains("active")) {
-    navSite.classList.add("active");
-    overlay.classList.add("active");
-  } else {
-    navSite.classList.remove("active");
-    overlay.classList.remove("active");
-  }
+    this.classList.toggle("active");
+    if (this.classList.contains("active")) {
+        navSite.classList.add("active");
+    } else {
+        navSite.classList.remove("active");
+    }
 };
 
-window.onclick = function (e) {
-  if (e.target == overlay) {
-    navSite.classList.remove("active");
-    overlay.classList.remove("active");
-    bars.classList.remove("active");
-  }
+window.onmouseup = function (e) {
+    if (!navSite.contains(e.target) && !bars.contains(e.target)) {
+        navSite.classList.remove("active");
+        bars.classList.remove("active");
+    }
 };
 
 let allLinks = document.querySelectorAll("ul li a.link-to");
 let block = document.querySelectorAll(".block");
 allLinks.forEach((el) => {
-  el.onclick = function (e) {
-    e.preventDefault();
-    allLinks.forEach((el) => el.classList.remove("active"));
-    this.classList.add("active");
-    scrollToElement(this);
-  };
+    el.onclick = function (e) {
+        e.preventDefault();
+        allLinks.forEach((el) => el.classList.remove("active"));
+        this.classList.add("active");
+        scrollToElement(this);
+    };
 });
 
 function toggleClass() {
-  block.forEach((el) => {
-    if (window.scrollY >= el.offsetTop) {
-      allLinks.forEach((el) => {
-        el.classList.remove("active");
-      });
-      document
-        .querySelector("ul.menu li a[data-scroll=" + el.id + "]")
-        .classList.add("active");
-    }
-  });
+    block.forEach((el) => {
+        if (window.scrollY >= el.offsetTop) {
+            allLinks.forEach((el) => {
+                el.classList.remove("active");
+            });
+            document.querySelector("ul.menu li a[data-scroll=" + el.id + "]").classList.add("active");
+        }
+    });
 }
 window.addEventListener("scroll", toggleClass);
 let allMobileLinks = document.querySelectorAll(".nav-site ul li a.link-to");
 allMobileLinks.forEach((el) => {
-  el.onclick = function (e) {
-    e.preventDefault();
-    allMobileLinks.forEach((el) => el.classList.remove("activeMobile"));
-    this.classList.add("activeMobile");
-    scrollToElement(el);
-  };
+    el.onclick = function (e) {
+        e.preventDefault();
+        allMobileLinks.forEach((el) => el.classList.remove("activeMobile"));
+        this.classList.add("activeMobile");
+        scrollToElement(el);
+    };
 });
 
 function toggleClassMobile() {
-  block.forEach((el) => {
-    if (window.scrollY >= el.offsetTop) {
-      allMobileLinks.forEach((el) => {
-        el.classList.remove("activeMobile");
-      });
-      document
-        .querySelector(".nav-site ul li a[data-scroll=" + el.id + "]")
-        .classList.add("activeMobile");
-    }
-  });
+    block.forEach((el) => {
+        if (window.scrollY >= el.offsetTop) {
+            allMobileLinks.forEach((el) => {
+                el.classList.remove("activeMobile");
+            });
+            document.querySelector(".nav-site ul li a[data-scroll=" + el.id + "]").classList.add("activeMobile");
+        }
+    });
 }
 window.addEventListener("scroll", toggleClassMobile);
 
 // scroll to element when click on link
 function scrollToElement(el) {
-  let goScroll = document.querySelector(
-    "#" + el.getAttribute("data-scroll")
-  ).offsetTop;
-  scrollTo({
-    top: goScroll,
-    behavior: "smooth",
-  });
+    let goScroll = document.querySelector("#" + el.getAttribute("data-scroll")).offsetTop;
+    scrollTo({
+        top: goScroll,
+        behavior: "smooth"
+    });
 }
+
+let closeBars = document.querySelector(".nav-site .bars");
+closeBars.onclick = function () {
+    navSite.classList.remove("active");
+    bars.classList.remove("active");
+};
 
 let counterCount = document.querySelector(".counter");
 let countIncrease = document.querySelectorAll(".count");
 let stopCounter = false;
+let speed = 100;
 
 function scrolledCounter() {
-  if (this.scrollY >= counterCount.offsetTop - 350) {
-    if (!stopCounter) countIncrease.forEach((el) => counter(el));
-    stopCounter = true;
-  }
+    if (
+        this.scrollY >= counterCount.offsetTop - 350 &&
+        this.scrollY <= counterCount.offsetTop + counterCount.offsetHeight - 350
+    ) {
+        if (!stopCounter) countIncrease.forEach((el) => counter(el));
+        stopCounter = true;
+    }
 }
+
 window.addEventListener("scroll", scrolledCounter);
 
 function counter(el) {
-  let getData = Number(el.getAttribute("data-count"));
-  setInterval(() => {
-    if (el.textContent < getData) el.textContent++;
-  }, 4500 / getData);
+    setInterval(() => {
+        let target = Number(el.getAttribute("data-count"));
+        let numContent = Number(el.textContent);
+        let increase = target / speed ;
+        if (target > numContent) {
+            el.textContent = Math.floor(increase + numContent);
+        }
+    });
 }
+
 
 let goUp = document.querySelector(".up");
 
 window.onscroll = function () {
-  goUp.classList.toggle('active', this.scrollY >= 550);
-}
+    goUp.classList.toggle("active", this.scrollY >= 550);
+};
 
 goUp.onclick = function () {
-  scrollTo ({
-    top: 0,
-    behavior: 'smooth'
-  })
-}
+    scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
 
 let copyRight = document.querySelector(".copy-right span");
-copyRight.innerHTML = new Date().getFullYear()
+copyRight.innerHTML = new Date().getFullYear();
